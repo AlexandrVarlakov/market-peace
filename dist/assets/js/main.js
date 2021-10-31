0636ec7a114e44e7176845ae0620632cc7cefd52
AlexandrVarlakov <66496459+AlexandrVarlakov@users.noreply.github.com>
2021-10-31 19:48:31 +0300

    add reviews section

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,18 +1,12 @@
-'use strict';
-
-
-let d = new Date(2012, 1, 20, 3, 12);
-
-let week = ['ПТ', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
-
-function getEuropeDay(num){
-    if (num == 0) {
-        num = 6;
-    } else {
-        num--;
-    }
-
-    return num;
-}
-
-console.log(week[getEuropeDay(d.getDay())]);
+const swiper = new Swiper('.swiper', {
+    
+    slidesPerView: 'auto',
+    speed: 1000,
+  
+      navigation:{
+        nextEl: '.reviews__next',
+        prevEl: '.reviews__prev'
+      }, 
+    
+  });
+  
