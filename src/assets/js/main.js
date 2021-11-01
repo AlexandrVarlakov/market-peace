30f34ad92e19b183bc7980b356eac39cc390ca98
AlexandrVarlakov <66496459+AlexandrVarlakov@users.noreply.github.com>
2021-11-01 03:27:32 +0300

    main page witout media

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,3 +10,52 @@ const swiper = new Swiper('.swiper', {
     
   });
   
+
+  const swiper_plain = new Swiper('.swiper-price-plan', {
+    
+    slidesPerView: 'auto',
+    speed: 1000,
+
+    watchOverflow: true,
+    
+      navigation:{
+        nextEl: '.reviews__next',
+        prevEl: '.reviews__prev'
+      }, 
+    
+  });
+  
+  let options = {
+    //zIndex: 1000, 
+    background: 'rgba(0, 0, 0, 0.7)', 
+    //displayFog: 'block', //Значение по умолчанию flex
+    //displayModal: 'flex', //Значение по умолчанию block
+    //showModalAnimationName: 'fadeInBottom', 
+    //closeModalAnimationName: 'fadeOutTop', 
+    closeClasses: ['close-btn-wrap', 'close-btn'], 
+    //closeModalOnFogClick: false, 
+    showModalAnimationDuration: 800,
+    //closeModalAnimationDuration: 300,
+    showFogAnimationName: 'fadeInBottom',
+    closeFogAnimationName: 'fadeOutTop',
+    showFogAnimationDuration: 700,
+    closeFogAnimationDuration: 700,
+
+    documentScrolled: true, 
+    //onModalClose: function(){console.log('modal close');},
+    //onModalOpen: function(){console.log('modal open');}
+
+}
+
+document.querySelectorAll('.btn-entry').forEach( (item) => {
+    item.onclick = function(){
+        let modal = new easyModal('modal-entry', options);
+    }
+
+} ) 
+document.querySelectorAll('.btn-question').forEach( (item) => {
+    item.onclick = function(){
+        let modal = new easyModal('modal-question', options);
+    }
+
+} ) 
